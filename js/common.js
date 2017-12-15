ed087a3da2762c2d37e3bf613203e9df21899b5f
이상준 <sangjun.lee@titanplatform.net>
2017-12-15 17:58:47 +0900

    상단 헤더 스크립트 추가

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
+/* Header show&off */
+let header = document.querySelector('header');
+let memoryY = 0;
+window.addEventListener('scroll', function(){
+	let presentYoffset = window.scrollY;
+
+	if( (memoryY-presentYoffset) < 0) {
+		header.classList.add('scree_out');
+		memoryY = presentYoffset;
+	} else {
+		header.classList.remove('scree_out');
+		memoryY = presentYoffset;
+	}
+});
+
+
+
+
+
 /*lnb menu button*/
 $('.btn_lnb_menu').on('click', function(){
 		if( $(this).hasClass('on') ) {
@@ -8,3 +27,19 @@ $('.btn_lnb_menu').on('click', function(){
 			$('.lnb').addClass('on');
 		}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
